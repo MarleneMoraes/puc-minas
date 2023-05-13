c47ca79f757189226dbfb4dde8e1a50537c51aa7
Marlene Moraes <marlenevmoraes@outlook.com>
2023-05-12 22:50:05 -0300

    bubblesort nba

--- algoritmos-e-estrutura-de-dados-ii/src/br/pucminas/aedsii/jogadornba/metodosdeordenacao/bubble/Main.java
+++ algoritmos-e-estrutura-de-dados-ii/src/br/pucminas/aedsii/jogadornba/metodosdeordenacao/bubble/Main.java
@@ -1,9 +1,6 @@
 package br.pucminas.aedsii.jogadornba.metodosdeordenacao.bubble;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 import br.pucminas.aedsii.estruturadedados.MyIO;
@@ -12,62 +9,114 @@ import br.pucminas.aedsii.jogadornba.Jogador;
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		MyIO.setCharset("UTF-8");
-		Jogador[] jogadores = new Jogador[4000];
-		Scanner arqLeitura = new Scanner(new File("jogadores.txt"));
-
-		int i = -1;
-		// Leitura de arquivo
-		while (arqLeitura.hasNextLine()) {
-			String linha = arqLeitura.nextLine();
-			if (i != -1) {
-				jogadores[i] = Jogador.ler(linha);
-			}
-			i++;
-		}
-		arqLeitura.close();
-
-		// Busca do jogador
-		String entrada = MyIO.readLine();
-		Jogador[] jogadoresOrdenados = new Jogador[1000];
-		int tamanho = 0;
-
-		for (int j = 0; !isFim(entrada); j++) {
-			int id = Integer.parseInt(entrada);
-
-			for (Jogador jogador : jogadores) {
-				if (jogador.verificarJogador(id)) {
-					jogadoresOrdenados[j] = jogador;
-					tamanho++;
-				}
-			}
-			entrada = MyIO.readLine();
-		}
+		Scanner scan = new Scanner(System.in);
+		Jogador[] jogadoresOrdenados = armazenarJogadores(scan);
 
 		// Bubble Sort
 		int troca = 0;
 		int comparacao = 0;
 
-		for (int k = (tamanho - 1); k > 0; k--) {
-			for (int j = 0; j < i; j++) {
+		for (int k = (jogadoresOrdenados.length - 1); k > 0; k--) {
+			for (int j = 0; j < k; j++) {
 				comparacao++;
 
 				if ((jogadoresOrdenados[j].getCidadeNascimento().compareTo(jogadoresOrdenados[j + 1].getCidadeNascimento()) > 0)
 						|| ((jogadoresOrdenados[j].getCidadeNascimento().compareTo(jogadoresOrdenados[j + 1].getCidadeNascimento()) == 0)
-								&& (jogadoresOrdenados[j].getNome().compareTo(jogadores[j + 1].getNome()) > 0))) {
-					Jogador temp = jogadoresOrdenados[j];
-					jogadoresOrdenados[j] = jogadoresOrdenados[j + 1];
-					jogadoresOrdenados[j + 1] = temp;
+								&& (jogadoresOrdenados[j].getNome().compareTo(jogadoresOrdenados[j + 1].getNome()) > 0))) {
+					troca(jogadoresOrdenados, j);
 					troca++;
 				}
 			}
 		}
 		
 		// Impressao dos jogadores ordenados
-		for (Jogador jogador : jogadoresOrdenados) {
+		for (Jogador jogador : jogadoresOrdenados) 
 			jogador.imprimir();
+		
+		criarArquivoLog(troca, comparacao);
+		
+	}
+
+	// Busca jogador pelo id
+	public static Jogador buscarJogador(Jogador[] jogador, int id) {
+		for (int i = 0; i < jogador.length; i++) {
+			if (jogador[i] == null)
+				continue;
+
+			if (jogador[i].verificarJogador(id))
+				return jogador[i];
 		}
 
-		// Arquivo log de armazenamento
+		return null;
+	}
+
+	// Verificacao da palavra "FIM", na entrada
+	public static boolean isFim(String s) {
+		return (s.length() == 3 && s.equals("FIM"));
+	}
+
+	// Preenchimento do vetor principal atraves da leitura de arquivos
+	public static Jogador[] preencherJogadores() throws FileNotFoundException {
+		Jogador[] jogadores = new Jogador[4000];
+		Scanner arqLeitura = new Scanner(new File("jogadores.txt"));
+
+		int i = -1;
+
+		while (arqLeitura.hasNextLine()) {
+			String linha = arqLeitura.nextLine();
+			if (i != -1)
+				jogadores[i] = Jogador.ler(linha);
+			
+			i++;
+		}
+		arqLeitura.close();
+
+		return jogadores;
+	}
+	
+	// Armazenamento de jogadores encontrados para realizacao do bubblesort
+	public static Jogador[] armazenarJogadores(Scanner scan) throws FileNotFoundException {
+		String entrada = scan.nextLine();
+		Jogador[] jogadores = preencherJogadores();
+		Jogador[] jogadoresEncontrados = new Jogador[4000];
+		int tamanho = 0;
+		
+		for (int j = 0; !isFim(entrada); j++) {
+			int id = Integer.parseInt(entrada);
+
+			Jogador jogador = buscarJogador(jogadores, id);
+			
+				if (jogador != null) {
+					tamanho++;
+					jogadoresEncontrados[j] = jogador;
+				}
+			
+			entrada = scan.nextLine();
+		}
+		
+		return construirVetor(jogadoresEncontrados, tamanho);	
+	}
+	
+	// Construcao do vetor para realizar no bubble com tamanho exato
+	public static Jogador[] construirVetor(Jogador[] jogadoresEncontrados, int tamanho) {
+		Jogador[] jogadoresOrdenados = new Jogador[tamanho];
+		
+		for(int i = 0; i < jogadoresOrdenados.length; i++) 
+			jogadoresOrdenados[i] = jogadoresEncontrados[i];
+		
+		
+		return jogadoresOrdenados;
+	}
+	
+	// Metodo para trocar os jogadores
+	public static void troca(Jogador[] jogadores, int i) {
+		Jogador temp = jogadores[i];
+		jogadores[i] = jogadores[i + 1];
+		jogadores[i + 1] = temp;
+	}
+	
+	// Criacao de arquivo de log de armazenamento
+	public static void criarArquivoLog(int troca, int comparacao) {
 		long tempoExecucao = System.currentTimeMillis();
 
 		try {
@@ -81,19 +130,5 @@ public class Main {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-	}
-
-	public static void buscarJogador(Jogador[] jogador, int id) {
-		for (int i = 0; i < jogador.length; i++) {
-			if (jogador[i] == null)
-				continue;
-
-			if (jogador[i].verificarJogador(id))
-				jogador[i].imprimir();
-		}
-	}
-
-	public static boolean isFim(String s) {
-		return (s.length() == 3 && s.equals("FIM"));
 	}
 }
